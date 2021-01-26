d0e850286332e204e76f10fa042ab2fa10f3e4c7
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-01-26 14:50:04 -0800

    creating row component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Row from './components/Row/Row.component';
 
 function App() {
   return (
     <div className="App">
       <h1>testing</h1>
+      <Row title="NETFLIX ORIGINALS" />
+      <Row title="Trending Now" />
     </div>
   );
 }
