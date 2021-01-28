255e29316f9678c1e02b4a79f67b17f9aebe7fe8
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-01-28 14:56:40 -0800

    styling for the row

--- src/components/Row/Row.component.js
+++ src/components/Row/Row.component.js
@@ -21,7 +21,7 @@ function Row({ title, fetchUrl }) {
     return (
         <div className="row">
             <h2>{title}</h2>
-            <div className="row_posters">
+            <div className="row__posters">
                 {movies.map(
                     (movie) =>
                         <img
