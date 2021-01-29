472af7901bd6a5ef531ec9ba4103da2115fa1fe6
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-01-29 14:17:43 -0800

    fetching data for banner background

--- src/components/Banner/Banner.component.js
+++ src/components/Banner/Banner.component.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from '../../axios/axios';
+import requests from '../../request/request';
+import './Banner.styles.css'
 
 function Banner() {
-    return (
-        <div>
+    const [movie, setMovie] = useState([]);
 
-        </div>
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            );
+            return request;
+        }
+
+        fetchData()
+    }, [])
+
+    return (
+        <header className="banner"
+            style={{
+                backgroundSize: "cover",
+                backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+                backdropPosition: "center center",
+            }}>
+            <div className="banner_contents">
+                {/* title */}
+                <h1 className="banner_title">
+                    {movie?.title || movie?.name || movie?.original_name}
+                </h1>
+
+                {/* 2 buttons */}
+                <div className="banner_buttons">
+                    <button className="banner_button">Play</button>
+                    <button className="banner_button">My List </button>
+                </div>
+
+                {/* description */}
+                {/* <h1 className="banner_description">{truncate(movie?.overview, 200)}</h1> */}
+            </div>
+        </header>
     )
 }
 
