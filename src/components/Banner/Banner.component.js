35ebef54472e79120bb7778def93355f4cb58764
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-02-01 13:10:10 -0800

    adding fade to the bottom of the banner

--- src/components/Banner/Banner.component.js
+++ src/components/Banner/Banner.component.js
@@ -20,6 +20,10 @@ function Banner() {
         fetchData()
     }, [])
 
+    function truncate(str, n) {
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+    }
+
     return (
         <header className="banner"
             style={{
@@ -39,10 +43,11 @@ function Banner() {
                     <button className="banner__button">My List </button>
                 </div>
 
-                <h1 className="banner__description">{movie?.overview}</h1>
+
                 {/* description */}
-                {/* <h1 className="banner_description">{truncate(movie?.overview, 200)}</h1> */}
+                <h1 className="banner__description">{truncate(movie?.overview, 200)}</h1>
             </div>
+            <div className="banner__fadeBottom" />
         </header>
     )
 }
